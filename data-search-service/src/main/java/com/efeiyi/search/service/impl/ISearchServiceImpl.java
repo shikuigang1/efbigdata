b05ee79399d29c1f14979be73d950f1364825690
shikuigang <slting12@163.com>
2017-04-24 16:11:51 +0800

    先了解

--- data-search-service/src/main/java/com/efeiyi/search/service/impl/ISearchServiceImpl.java
+++ data-search-service/src/main/java/com/efeiyi/search/service/impl/ISearchServiceImpl.java
@@ -9,7 +9,9 @@ import java.util.List;
  * Created by Administrator on 2017/4/6 0006.
  */
 public class ISearchServiceImpl implements ISearchService{
+
     public List searchText(String text) {
         return null;
     }
+
 }
